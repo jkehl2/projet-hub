b2fb4611a289d33c81be97b7189ee0087e690bfa
yannsaintmarc <yann.saintmarc@gmail.com>
2021-01-20 21:21:04 +0100

    creation du composant SignIn - WIP

--- front/src/components/SignIn/index.js
+++ front/src/components/SignIn/index.js
@@ -1,24 +1,52 @@
 // == Import npm
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import {
+  Container, Header, Form, Input, Button,
+} from 'semantic-ui-react';
 // == IMPORTS COMPOSANTS
 
 // == IMPORTS CONTAINERS
 
 // == STYLES
-import './signIn.scss';
+import './SignIn.scss';
 
 // == Composant
-const App = ({ props }) => {
-  return (
-    <div className="SignIn">
-    </div>
-  );
-};
+const SignIn = (
+  {
+    email,
+    password,
+    handleLogin,
+    handleSignIn,
+  },
+) => (
+  <Container className="SignIn">
+    {/** titre de la page */}
+    <Header as="h1">Login</Header>
+    {/** formulaire d'identification */}
+    <Form>
+      {/** email */}
+      <Form.Field>
+        <Input type="text" name="email" placeholder="monemail@domain.foo" value={email} />
+      </Form.Field>
+      {/** mot de passe */}
+      <Form.Field>
+        <Input type="text" name="password" placeholder="mot de passe" value={password} />
+      </Form.Field>
+    </Form>
+    {/** bouton connection */}
+    <Button class="medium ui button" type="submit" onClick={handleLogin}>connection</Button>
+    <div className="ui divider" />
+    {/** bouton inscription */}
+    <Button class="medium ui button" type="submit" onClick={handleSignIn}>inscription</Button>
+  </Container>
+);
 
-App.propTypes = {
-  props: PropTypes.object.isRequired,
+SignIn.propTypes = {
+  email: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
+  handleLogin: PropTypes.func.isRequired,
+  handleSignIn: PropTypes.func.isRequired,
 };
 
 // == Export
