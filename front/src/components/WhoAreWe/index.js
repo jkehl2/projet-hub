35f194ae14c43f7f1167d6cfac6b497ab7b2a48b
wickwire-87 <wickwire1987@gmail.com>
2021-01-20 11:33:13 +0100

    maj WhoWeAre

---
+++ front/src/components/WhoAreWe/index.js
@@ -0,0 +1,25 @@
+// == Import npm
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// == IMPORTS COMPOSANTS
+
+// == IMPORTS CONTAINERS
+
+// == STYLES
+import './app.scss';
+
+// == Composant
+const App = ({ props }) => {
+  return (
+    <div className="app">
+    </div>
+  );
+};
+
+App.propTypes = {
+  props: PropTypes.object.isRequired,
+};
+
+// == Export
+export default
