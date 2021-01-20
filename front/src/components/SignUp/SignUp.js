abe2abbe1c41ce016a0e5f1f8ef16ead520cd9d2
yannsaintmarc <yann.saintmarc@gmail.com>
2021-01-20 13:07:44 +0100

    creation du dossier SignUp

---
+++ front/src/components/SignUp/SignUp.js
@@ -0,0 +1,25 @@
+// == Import npm
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// == IMPORTS COMPOSANTS
+
+// == IMPORTS CONTAINERS
+
+// == STYLES
+import './SignUp.scss';
+
+// == Composant
+const App = ({ props }) => {
+  return (
+    <div className="SignUp">
+    </div>
+  );
+};
+
+App.propTypes = {
+  props: PropTypes.object.isRequired,
+};
+
+// == Export
+export default SignUp;
