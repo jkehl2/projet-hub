42c5ea7e6f1185e5762accf81849edd9aa9f5f90
LaraMi92 <laraminski@hotmail.fr>
2021-01-20 14:35:01 +0100

    home composant

--- front/src/components/Home/index.js
+++ front/src/components/Home/index.js
@@ -1,6 +1,9 @@
 // == Import npm
 import React from 'react';
 import PropTypes from 'prop-types';
+import {
+  Container, Card, Image, Form, Input, Icon,
+} from 'semantic-ui-react';
 
 // == IMPORTS COMPOSANTS
 
@@ -11,7 +14,34 @@ import './home.scss';
 
 // == Composant
 const Home = ({ props }) => (
-  <div className="home" />
+  <div className="home">
+    <Container>
+      {/** Page top with pictures */}
+      <Card.Group>
+        <Card>
+          <Image />
+        </Card>
+      </Card.Group>
+      {/** Page header with Local Hub presentation */}
+      <Card>
+        <Card.Header>Local Hub</Card.Header>
+        <Card.Description>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Duis aute irure dolor in reprehenderit
+          in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Excepteur sint occaecat cupidatat non proident,
+          sunt in culpa qui officia deserunt mollit anim id est laborum."
+        </Card.Description>
+      </Card>
+      {/** Search Bar */}
+      <Form>
+        <Input placeholder="Search" />
+        <Icon name="search" />
+      </Form>
+    </Container>
+  </div>
 );
 
 Home.propTypes = {
