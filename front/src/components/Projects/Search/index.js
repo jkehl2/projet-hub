f89855187d20e988b5a823b943b1e7fe2af0e9f7
wickwire-87 <wickwire1987@gmail.com>
2021-01-20 18:22:03 +0100

    partie search de projects (non finie)

--- front/src/components/Projects/Search/index.js
+++ front/src/components/Projects/Search/index.js
@@ -2,7 +2,9 @@
 import React from 'react';
 
 // == IMPORTS COMPOSANTS
-
+import {
+  Container, Header, Button, Form, 
+} from 'semantic-ui-react';
 // == IMPORTS CONTAINERS
 
 // == STYLES
@@ -10,7 +12,37 @@ import './search.scss';
 
 // == Composant
 const Search = () => (
-  <div className="search" />
+  <container className="search" />
+  {/* titre */}
+  <Header as="h1">Effectuer une recherche</Header>
+  {/* localité search bar*/}
+  <FormField>
+    <Input action={{ icon: 'search' }} placeholder='Search...' />
+  </FormField>
+  {/* range */}
+<Grid columns={2}>
+<Grid.Column as={Form}>
+<Form.Input
+label={`Range: ${distance}km`}
+min={1}
+max={10}
+name='distance'
+onChange={this.handleChange}
+step={1}
+type='range'
+value={distance}
+/>
+<Form.Button
+content={visible ? 'Unmount' : 'Mount'}
+onClick={this.handleVisibility}
+/>
+</Grid.Column>
+</Grid>
+  {/* button Include archives */}
+
+  {/* bouton valider */}
+  <Button positive>Valider</Button>
+  </container>
 );
 
 // == Export
