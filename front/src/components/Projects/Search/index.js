48fe1d3aa0685a29fa5534e7e728bf7697f3b9b1
wickwire-87 <wickwire1987@gmail.com>
2021-01-20 18:58:14 +0100

    partie search done

--- front/src/components/Projects/Search/index.js
+++ front/src/components/Projects/Search/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 // == IMPORTS COMPOSANTS
 import {
-  Container, Header, Button, Form, 
+  Container, Header, Button, Form, Grid,
 } from 'semantic-ui-react';
 // == IMPORTS CONTAINERS
 
@@ -13,35 +13,41 @@ import './search.scss';
 // == Composant
 const Search = () => (
   <container className="search" />
-  {/* titre */}
-  <Header as="h1">Effectuer une recherche</Header>
-  {/* localité search bar*/}
-  <FormField>
-    <Input action={{ icon: 'search' }} placeholder='Search...' />
-  </FormField>
-  {/* range */}
-<Grid columns={2}>
-<Grid.Column as={Form}>
-<Form.Input
-label={`Range: ${distance}km`}
-min={1}
-max={10}
-name='distance'
-onChange={this.handleChange}
-step={1}
-type='range'
-value={distance}
-/>
-<Form.Button
-content={visible ? 'Unmount' : 'Mount'}
-onClick={this.handleVisibility}
-/>
-</Grid.Column>
-</Grid>
-  {/* button Include archives */}
-
-  {/* bouton valider */}
-  <Button positive>Valider</Button>
+    {/* titre */}
+    <Header as="h1">Effectuer une recherche</Header>
+      {/* localité search bar*/}
+        <FormField>
+          <Input action={{ icon: 'search' }} placeholder='Search...' />
+        </FormField>
+          {/* range */}
+            <Grid columns={2}>
+              <Grid.Column as={Form}>
+                <Form.Input
+                  label={`Range: ${distance}km`}
+                  min={1}
+                  max={10}
+                  name='distance'
+                  onChange={this.handleChange}
+                  step={1}
+                  type='range'
+                  value={distance}
+                  />
+                    <Form.Button
+                      content={visible ? 'Unmount' : 'Mount'}
+                        onClick={this.handleVisibility}
+                    />
+              </Grid.Column>
+            </Grid>
+                          {/* button Include archives */}
+                            <div>
+                              Inclure les projets archivés:
+                              <Button toggle active={active} onClick={this.handleClick}>
+                                Toggle
+                              </Button>
+                            </div>
+                                {/* bouton valider */}
+                                
+                                  <Button positive>Valider</Button>
   </container>
 );
 
