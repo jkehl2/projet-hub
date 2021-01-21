a111957f307c58235891a12f7de11b61cdb87178
LaraMi92 <laraminski@hotmail.fr>
2021-01-21 14:29:15 +0100

    actions store

--- front/src/store/actions.js
+++ front/src/store/actions.js
@@ -1,3 +1,11 @@
 /* eslint-disable object-shorthand */
 
 export const CREATE_PROJECT = 'CREATE_PROJECT';
+
+// action creator
+
+// uploading a project
+export const createProject = (projectData) => ({
+  type: CREATE_PROJECT,
+  quer,
+});
