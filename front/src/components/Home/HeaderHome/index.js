846741a30ef18eeb9e3a7352d38dcafe064846d7
LaraMi92 <laraminski@hotmail.fr>
2021-01-24 13:18:49 +0100

    homepage cards

--- front/src/components/Home/HeaderHome/index.js
+++ front/src/components/Home/HeaderHome/index.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 // == IMPORTS COMPOSANTS
 import {
-  Card, Image,
+  Card, Image, Segment,
 } from 'semantic-ui-react';
 
 // == IMPORTS CONTAINERS
@@ -15,14 +15,71 @@ import './headerHome.scss';
 // == Composant
 const HeaderHome = () => (
   <Card.Group itemsPerRow="3">
-    <Card>
-      <Image src="https://image.shutterstock.com/z/stock-vector-business-concept-team-metaphor-people-connecting-puzzle-elements-vector-illustration-flat-design-1390292588.jpg" />
+    <Card color="teal">
+      <Card.Content
+        style={{
+          backgroundColor: '#AFEEEE',
+        }}
+      >
+        <Card.Header
+          as="h1"
+          textAlign="center"
+          style={{
+            border: '1px solid teal',
+            width: '2em',
+            height: '2em',
+            borderRadius: '50%',
+            boxShadow: '.2em .1em .1em teal',
+            color: 'teal',
+          }}
+        >1.
+        </Card.Header>
+        <Card.Header as="h2" textAlign="center" style={{ color: '#00CED1' }}>Poster</Card.Header>
+      </Card.Content>
     </Card>
-    <Card>
-      <Image src="https://image.shutterstock.com/z/stock-vector-business-concept-team-metaphor-people-connecting-puzzle-elements-vector-illustration-flat-design-1240181551.jpg" />
+    <Card color="teal">
+      <Card.Content
+        style={{
+          backgroundColor: '#AFEEEE',
+        }}
+      >
+        <Card.Header
+          as="h1"
+          textAlign="center"
+          style={{
+            border: '1px solid teal',
+            width: '2em',
+            height: '2em',
+            borderRadius: '50%',
+            boxShadow: '.2em .1em .1em teal',
+            color: 'teal',
+          }}
+        >2.
+        </Card.Header>
+        <Card.Header as="h2" textAlign="center" style={{ color: '#00CED1' }}> Echanger</Card.Header>
+      </Card.Content>
     </Card>
-    <Card>
-      <Image src="https://image.shutterstock.com/z/stock-vector-teamwork-concept-vector-illustration-employees-assemble-a-puzzle-symbolizing-parts-of-an-effective-1490896448.jpg" />
+    <Card color="teal">
+      <Card.Content
+        style={{
+          backgroundColor: '#AFEEEE',
+        }}
+      >
+        <Card.Header
+          as="h1"
+          textAlign="center"
+          style={{
+            border: '1px solid teal',
+            width: '2em',
+            height: '2em',
+            borderRadius: '50%',
+            boxShadow: '.2em .1em .1em teal',
+            color: 'teal',
+          }}
+        >3.
+        </Card.Header>
+        <Card.Header as="h2" textAlign="center" style={{ color: '#00CED1' }}> Collaborer</Card.Header>
+      </Card.Content>
     </Card>
   </Card.Group>
 );
