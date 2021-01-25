c7dfcb1a6ae93f4da6e9adcbc5992f653ca68351
wickwire-87 <wickwire1987@gmail.com>
2021-01-25 14:23:07 +0100

    correction screw up

--- front/src/components/WhoAreWe/index.js
+++ front/src/components/WhoAreWe/index.js
@@ -31,7 +31,6 @@ const WhoAreWe = ({  }) => (
             header="Lara Minski"
             description="Lead dev front."
           />
-        </Grid.Column>
         <Grid.Column>
           <Card
             image src="https://ca.slack-edge.com/T018S94MBUY-U018HM615K7-efac29ea0437-512"
