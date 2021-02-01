430a56ac1e1c6b65f712a3fedf0d2fb192934be1
jkehl2 <kehl.pro@gmail.com>
2021-02-01 14:17:20 +0100

    CODE REVIEW - RESOLVE SOMES BUGS ON PROJECT EDIT ACTION AND PAGE

--- front/src/utils/dateFormater.js
+++ front/src/utils/dateFormater.js
@@ -1,10 +1,8 @@
 export default (date) => {
   let d = new Date(date);
-  if (Number.isNaN(d.getTime())) { // TEST IF IS INVALID DATE
-    console.log(date);
+  if (Number.isNaN(d.getTime())) { // TEST IF IS INVALID DATE -> Date au format dd/mm/yyyy
     const dateTbl = date.split('/');
     d = new Date(dateTbl[2], dateTbl[1] - 1, dateTbl[0], 0, 0, 0, 0);
-    console.log(date);
   }
   let month = `${d.getMonth() + 1}`;
   let day = `${d.getDate()}`;
