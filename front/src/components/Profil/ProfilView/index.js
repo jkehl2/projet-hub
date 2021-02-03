45a403c890e8d5c16ffe6dade20ed3a5de8212d3
jkehl2 <kehl.pro@gmail.com>
2021-02-03 19:31:10 +0100

    UPDATE - CSS STYLE ON PROFIL PAGE

--- front/src/components/Profil/ProfilView/index.js
+++ front/src/components/Profil/ProfilView/index.js
@@ -2,18 +2,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// == IMPORTS CONTAINERS
-
-// == IMPORTS COMPOSANTS
+// == IMPORTS COMPONENT
 import {
-  Button, Item, Segment,
+  Button,
+  Item,
+  Segment,
 } from 'semantic-ui-react';
 import ModalConfirm from 'src/components/ModalConfirm';
 
 // == IMPORT STYLES
 import './profilView.scss';
 
-// == Composant Profil mode consultation
+// == PRIMARY COMPONENT
 const ProfilView = ({
   name,
   email,
@@ -30,72 +30,85 @@ const ProfilView = ({
   redirectToMyProjects,
   redirectToMyFavorites,
 }) => (
-  <Segment textAlign="left">
+  <Segment className="profil-view--container" textAlign="left">
     <Item.Group relaxed>
-      <Item>
-        {/* avatar */}
+      <Item className="profil-view--no-margin-top">
+
+        {/* USER AVATAR */}
         <Item.Image size="small" src={`${avatar}`} />
         <Item.Content>
-          {/* pseudo */}
+
+          {/* USER NAME */}
           <Item.Header as="h2">{`${name}`}</Item.Header>
           <Item.Meta>
-            {/* email */}
+
+            {/* LINK TO USER EMAIL */}
             <a href={`mailto:${email}`}>{`${email}`}</a>
           </Item.Meta>
+
           <Item.Description>
-            <Button.Group>
-              {/* bouton mes projets */}
-              <Button
-                basic
-                color="green"
-                onClick={redirectToMyProjects}
-              >Mes projets
-              </Button>
-              {/* bouton mes favoris */}
-              <Button
-                basic
-                color="green"
-                onClick={redirectToMyFavorites}
-              >Mes favoris
-              </Button>
-            </Button.Group>
+            <div className="profil-view--centered-content">
+              <Button.Group>
+
+                {/* BUTTON - MY PROJECTS */}
+                <Button
+                  basic
+                  color="green"
+                  onClick={redirectToMyProjects}
+                >Mes projets
+                </Button>
+
+                {/* BUTTON - MY FAVORITES */}
+                <Button
+                  basic
+                  color="green"
+                  onClick={redirectToMyFavorites}
+                >Mes favoris
+                </Button>
+              </Button.Group>
+            </div>
           </Item.Description>
         </Item.Content>
       </Item>
     </Item.Group>
-    <Button.Group vertical>
-      {/* bouton modifer */}
-      <Button
-        basic
-        color="blue"
-        onClick={() => {
-          switchToEditProFile();
-        }}
-      >Modifier mes informations personnelles
-      </Button>
-      {/* Bouton Modification du mot de passe */}
-      <Button
-        className="profil-view--marged"
-        basic
-        color="orange"
-        onClick={redirectToPasswordEdit}
-      >Modification du mot de passe
-      </Button>
 
-      {/* bouton mes supprimer le compte */}
-      <ModalConfirm
-        title="Confirmer la suppression définitive de votre compte utilisateur"
-        content="Attention, la suppression de votre compte entrainera la suppression de toutes les fiches projets (en cours, archivées) dont vous êtes l'auteur."
-        trigger={<Button className="profil-view--marged" negative>Supprimer votre compte</Button>}
-        confirm={confirm}
-        setConfirmation={setConfirmation}
-        handleAction={deleteProfil}
-        isError={isError}
-        error={error}
-        isMessage={isMessage}
-        message={message}
-      />
-    </Button.Group>
+    <div className="profil-view--centered-content">
+      <Button.Group vertical>
+
+        {/* BUTTON - ACCOUNT EDIT */}
+        <Button
+          basic
+          color="blue"
+          onClick={() => {
+            switchToEditProFile();
+          }}
+        >Modifier mes informations personnelles
+        </Button>
+
+        {/* BUTTON - PASSWORD EDIT */}
+        <Button
+          className="profil-view--marged"
+          basic
+          color="orange"
+          onClick={redirectToPasswordEdit}
+        >Modification du mot de passe
+        </Button>
+
+        {/* BUTTON - MODAL - DELETE ACCOUNT */}
+        <ModalConfirm
+          title="Confirmer la suppression définitive de votre compte utilisateur"
+          content="Attention, la suppression de votre compte entrainera la suppression de toutes les fiches projets (en cours, archivées) dont vous êtes l'auteur."
+          trigger={<Button className="profil-view--marged" negative>Supprimer votre compte</Button>}
+          confirm={confirm}
+          setConfirmation={setConfirmation}
+          handleAction={deleteProfil}
+          isError={isError}
+          error={error}
+          isMessage={isMessage}
+          message={message}
+        />
+      </Button.Group>
+    </div>
   </Segment>
 );
 
